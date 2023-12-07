b7dff5423d1c90750a2363f4900d7a6aeba2d1fc
OsmnCnG <canos1997@hotmail.com>
2023-12-07 03:56:59 +0300

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Test from "./Test";
+import "./app.scss"
+import Contact from "./components/contact/Contact";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+    </section>
+    <section id="Services">
+      <Parallax type="services"/>
+    </section>
+    <section id="About">
+      <Services/>
+    </section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    {/* <section>Portfolio1</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section> */}
+    <section id="Contact">
+      <Contact/>
+    </section>
 
-export default App
+
+
+  </div>;
+};
+
+export default App;
